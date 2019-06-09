fee0c980efcc4fb64ded5a18cbb8f338b83b5ee8
maHlavacek <martin_hlavacek@icloud.com>
2019-06-09 10:03:09 +0200

    ImportConsole implemented

--- ClubAdministration.ImportConsole/ImportController.cs
+++ ClubAdministration.ImportConsole/ImportController.cs
@@ -13,7 +13,35 @@ namespace ClubAdministration.ImportConsole
 
         public static MemberSection[] ReadFromCsv()
         {
-            throw new NotImplementedException();
+            string[][] matrix = MyFile.ReadStringMatrixFromCsv(Filename, false);
+
+            var members = matrix
+                         .GroupBy(line => line[0] + '_' + line[1])
+                         .Select(grp => new Member
+                         {
+                             FirstName = grp.First()[1],
+                             LastName = grp.First()[0]
+                         })
+                         .ToArray();
+
+            var sections = matrix
+                          .GroupBy(line => line[2])
+                          .Select(grp => new Section
+                          {
+                              Name = grp.Key
+                          })
+                          .ToArray();
+
+            var memberSections = matrix
+                                .Select(line => new MemberSection
+                                {
+                                    Member = members.Single(m => m.FirstName == line[1] && m.LastName == line[0]),
+                                    Section = sections.Single(s => s.Name == line[2])
+                                })
+                                .ToArray();
+
+            return memberSections;
+
         }
 
     }
